582ea211a42ef93eed25f19a18d1c7ea443c40fd
lmlmkp <1282788226@qq.com>
2021-03-07 20:41:42 +0800

    jdbc

---
+++ javaweb-jdbc/src/main/java/com/lmkp/test/JdbcTest.java
@@ -0,0 +1,34 @@
+package com.lmkp.test;
+
+import java.sql.*;
+
+public class JdbcTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 配置信息
+        // useUnicode=true&characterEncoding=utf-8 解决中文乱码问题
+        String url = "jdbc:mysql://119.23.252.239:3306/ashen_db?useUnicode=true&characterEncoding=utf-8";
+        String username = "root";
+        String password = "Lm0515kp(lmkp)";
+
+        // 1.加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2.连接数据库
+        Connection connection = DriverManager.getConnection(url, username, password);
+        // 3.向数据库发送SQL的对象Statement,PreparedStatement : CRUD
+        Statement statement = connection.createStatement();
+        // 4.编写SQL
+        String sql = "select * from USER";
+        // 5.执行SQL
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()){
+            System.out.println("id="+resultSet.getObject("id"));
+            System.out.println("name="+resultSet.getObject("user"));
+            System.out.println("password="+resultSet.getObject("password"));
+        }
+
+        // 6.关闭连接，释放资源（一定要做）先开后关
+        statement.close();
+        connection.close();
+    }
+}
---
+++ javaweb-jdbc/src/main/java/com/lmkp/test/JdbcTest2.java
@@ -0,0 +1,36 @@
+package com.lmkp.test;
+
+import java.sql.*;
+
+public class JdbcTest2 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 配置信息
+        // useUnicode=true&characterEncoding=utf-8 解决中文乱码问题
+        String url = "jdbc:mysql://119.23.252.239:3306/ashen_db?useUnicode=true&characterEncoding=utf-8";
+        String username = "root";
+        String password = "Lm0515kp(lmkp)";
+
+        // 1.加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2.连接数据库
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        // 3.编写SQL
+        String sql = "insert into USER(id,user,password) values(?,?,?)";
+        // 4.预编译
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1,3); // 给第一个占位符赋值
+        preparedStatement.setString(2,"cccc");
+        preparedStatement.setString(3,"lmlmlmlm");
+
+        // 5.执行SQL
+        int i = preparedStatement.executeUpdate();
+        if (i>0){
+            System.out.println("插入成功");
+        }
+
+        // 6.关闭连接，释放资源（一定要做）先开后关
+        preparedStatement.close();
+        connection.close();
+    }
+}
